d610949deffb8aeec46dfa131f93fd705b598945
Matt Im <matthew.seiim@gmail.com>
2020-03-20 17:19:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
 def reverse_each_word(string)
   myArray = string.split(/ /)
   myArray.each do |string|
-    newArray << string.reverse
+    newArray << string.reverse_each_word
+  end
+  reverse_each_word
 end
